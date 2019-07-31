5ceb444ce0618c96ebccd151f10218c57d138ade
Noah Rose Ledesma <RoseLedesma@ucdavis.edu>
2019-07-30 18:17:09 -0700

    Add pure virtual tile method and debug tile

--- Tile.h
+++ Tile.h
@@ -11,14 +11,31 @@ class Map;
 class AxialCoordinate;
 
 class Tile {
-public:
+ public:
   Tile( const Map* const map, const AxialCoordinate&& position );
   Tile( const Map* const map, const AxialCoordinate& position );
   virtual ~Tile();
 
-private:
-  // The position of this tile in the map.
-  const AxialCoordinate* position;
+  /*
+   * Returns the number of movement points this tile de/buffs from the piece
+   * which occupies it.
+   */
+
+  virtual int GetMovementModifier() = 0;
+ protected:
   // The map of which this tile belongs to
   const Map* const map;
+  // The position of this tile in the map.
+  const AxialCoordinate* position;
+};
+
+class DebugTile : public Tile {
+ public:
+  DebugTile( const Map* const map, const AxialCoordinate&& position );
+  DebugTile( const Map* const map, const AxialCoordinate& position );
+
+  int GetMovementModifier() override { return movementBonus; }
+
+protected:
+  const static int movementBonus = 4;
 };
