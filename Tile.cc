552a3c060da43046c99e5f005dff3f9a25775424
Noah Rose Ledesma <RoseLedesma@ucdavis.edu>
2019-07-30 18:17:18 -0700

    Create Tile.cc

---
+++ Tile.cc
@@ -0,0 +1,29 @@
+#include "Tile.h"
+
+/*
+ * Tile
+ */
+
+Tile::Tile( const Map* const map, const AxialCoordinate&& position ):
+    map(map), position(new AxialCoordinate(position)) {}
+
+Tile::Tile( const Map* const map, const AxialCoordinate& position ):
+    map(map), position(new AxialCoordinate(position)) {}
+
+/*
+ * ~Tile
+ */
+
+Tile::~Tile() {
+  delete position;
+}
+
+/*
+ * DebugTile
+ */
+DebugTile::DebugTile( const Map* const map, const AxialCoordinate&& position ):
+  Tile(map, position) {}
+DebugTile::DebugTile( const Map* const map, const AxialCoordinate& position ):
+  Tile(map, position) {}
+
+
