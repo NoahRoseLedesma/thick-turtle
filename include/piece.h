d03117a32a78225b53d60d4adec9d4559cb7aa47
Noah Rose Ledesma <RoseLedesma@ucdavis.edu>
2019-07-30 21:48:51 -0700

    Rework piece class

--- include/piece.h
+++ include/piece.h
@@ -6,13 +6,38 @@
 #pragma once
 
 #include "player.h"
+#include "tile.h"
 
 class Piece {
  public:
-    virtual void Move(const Tile& p_target_tile) = 0;
-    virtual void TakeDamage(const int& damage) = 0;
-    
- private:
-    Tile m_tile;
-    int m_hp;
+    /*
+     * Construct a piece and place it on the specified tile
+     */
+    Piece(const Tile* position);
+    /*
+     * Change which tile this piece is occuping
+     * TODO: What is the behavior of this method if the tile is invalid
+     * or the tile is already occupied?
+     */
+    virtual void Move(const Tile* p_target_tile);
+    /*
+     * Apply damage to the piece, killing it if HP drops below zero.
+     */
+    virtual void TakeDamage(size_t damage);
+    /*
+     * gets the HP this unit should have when constructed
+     */
+    virtual size_t GetStartingHP() const;
+    /*
+     * Kill this unit and remove it from the game. It is the player's
+     * reponsibility to remove the dead unit (deallocate it, etc).
+     */
+    virtual void Kill();
+    /*
+     * Returns true if this unit has been killed.
+     */
+    bool IsDead() const { return m_hp == 0; }
+ protected:
+    const Tile* m_tile;
+    size_t m_hp;
 };
--- src/piece.cc
+++ src/piece.cc
@@ -3,3 +3,39 @@
 //
 
 #include "piece.h"
+
+Piece::Piece(const Tile* position) {
+  Move(position);
+  m_hp = GetStartingHP();
+}
+
+void Piece::Move(const Tile* p_target_tile) {
+  // TODO: Notify the tile we are currently on that we are leaving
+  // and notify the tile we have landed on that we have arrived
+  m_tile = p_target_tile;
+}
+
+void Piece::TakeDamage(size_t damage) {
+  if( IsDead() ) {
+    // TODO: If this unit is dead and we apply damage to it, we should log this as
+    // a non-critical error.
+    return;
+  }
+
+  if( damage >= m_hp ) {
+    Kill();
+    return;
+  }
+
+  m_hp -= damage;
+}
+
+// Really should be purely virtual, however I want to use this in the default
+// constructor for all pieces.
+size_t Piece::GetStartingHP() const {
+  return 0;
+}
+
+void Piece::Kill() {
+  m_hp = 0;
+}
