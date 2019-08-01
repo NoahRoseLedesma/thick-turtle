8589c2d05f7549a9e47b54cce8e4d27d5389b838
stott531 <stephenott531@gmail.com>
2019-08-01 12:23:02 -0700

    Added a function to utility to determine if a vector contains an element

--- include/utility.h
+++ include/utility.h
@@ -4,4 +4,10 @@
 
 #pragma once
 
+#include <algorithm>
+#include <vector>
+
 size_t HexNumbers( const size_t n );
+
+template <typename T>
+bool VectorContains(const std::vector<T> &p_search_target, const T &element);
--- src/utility.cc
+++ src/utility.cc
@@ -10,7 +10,19 @@
  * Courtesy of The On-Line Encyclopedia of Integer Sequences
  * https://oeis.org/A003215
  */
-
 size_t HexNumbers( const size_t n ) {
     return 3*n*(n+1)+1;
+}
+/*
+ * Utility function to determine if a vector contains a given element
+ */
+template <typename T>
+bool VectorContains(const std::vector<T> &p_search_target, const T &element){
+    auto result = std::find(p_search_target.begin(), p_search_target.end(), element);
+    if( result == p_search_target.end() ){
+        return false;
+    }
+    else{
+        return true;
+    }
 }
