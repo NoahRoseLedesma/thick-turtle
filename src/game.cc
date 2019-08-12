da7a4ca509a2f16d156e1ada78db42dd30ffeffe
Noah Rose Ledesma <RoseLedesma@ucdavis.edu>
2019-08-12 14:45:43 -0700

    Put utility functions in game class

--- src/game.cc
+++ src/game.cc
@@ -4,6 +4,13 @@
 #include "map.h"
 #include "tile.h"
 
+/*
+ * Game Object
+ */
+
+/*
+ * Game::~Game
+ */
 Game::~Game() {
   if(map)
     delete map;
@@ -15,12 +22,17 @@ Game::~Game() {
   }
 }
 
-// TODO: Asset managment system
+/*
+ * Game::Game
+ */
 Game::Game() {
   // Create debug resources
   debugFont.loadFromFile("assets/fonts/FiraCode-Regular.ttf");
 }
 
+/*
+ * Game::InitMap
+ */
 void Game::InitMap( size_t radius ) {
   auto DebugTileProducer = [](Map* map, AxialCoordinate&& coord) -> Tile* {
     return new DebugTile(map, coord);
@@ -29,19 +41,28 @@ void Game::InitMap( size_t radius ) {
   map = new Map(radius, DebugTileProducer, this);
 }
 
+/*
+ * Game::InitWindow
+ */
 void Game::InitWindow( size_t desiredWidth, size_t desiredHeight ) {
   window = new sf::RenderWindow(sf::VideoMode(desiredWidth, desiredHeight),
                                 "Thick Turtle");
 }
 
+/*
+ * Game::GetWindowHeight
+ * Game::GetWindowWidth
+ */
 size_t Game::GetWindowHeight() const {
   return window->getSize().y;
 }
-
 size_t Game::GetWindowWidth() const {
   return window->getSize().x;
 }
 
+/*
+ * Game::Run
+ */
 void Game::Run() {
   while( window->isOpen() ) {
     sf::Event event;
@@ -56,10 +77,43 @@ void Game::Run() {
   }
 }
 
+/*
+ * Game::Think
+ */
 void Game::Think() {
   window->draw(*map);
 }
 
+/*
+ * Game::AxialToPixel
+ */
+sf::Vector2f Game::AxialToPixel(const AxialCoordinate& coordinate) const {
+  float x = GetTileRadius() * (1.5 * (float)coordinate.q) +
+            (float)GetWindowHeight()/2;
+  float y = GetTileRadius() * (((ROOT3 / 2) * coordinate.q)
+                               + (ROOT3 * coordinate.r));
+  return {x, y};
+}
+
+sf::Vector2f Game::AxialToPixel(const AxialCoordinate&& coordinate) const {
+  AxialCoordinate bind = coordinate;
+  return AxialToPixel(bind);
+}
+
+AxialCoordinate Game::PixelToAxial(size_t x, size_t y) const {
+  int q = (2/3) * (float)x / (float)GetTileRadius();
+  int r = (-1/3) * (float)x + (float)ROOT3/3 *
+                     (float)y / (float)GetTileRadius();
+  return  {q, r};
+}
+
+size_t Game::GetTileRadius() const {
+  // TODO: Determine this programatically
+  return 50;
+}
+/*
+ * Game::InitrenderTexture
+ */
 void Game::InitRenderTexture() {
   renderTexture.create(50, 50);
 }
