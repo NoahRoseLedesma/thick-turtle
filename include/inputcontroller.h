9637f47a503c03d6a97871c48391673da06c35a5
stott531 <stephenott531@gmail.com>
2019-08-17 23:07:52 -0700

    Created the input controller src and header files

---
+++ include/inputcontroller.h
@@ -0,0 +1,14 @@
+//
+// Created by stott on 8/17/19.
+//
+
+#ifndef THICK_TURTLE_INPUTCONTROLLER_H
+#define THICK_TURTLE_INPUTCONTROLLER_H
+
+
+class InputController {
+
+};
+
+
+#endif //THICK_TURTLE_INPUTCONTROLLER_H
---
+++ src/inputcontroller.cc
@@ -0,0 +1,5 @@
+//
+// Created by stott on 8/17/19.
+//
+
+#include "inputcontroller.h"
