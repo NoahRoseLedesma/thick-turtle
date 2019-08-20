ce9c6569ab9155dedd0e3bc5fc1edeba39062d9e
stott531 <stephenott531@gmail.com>
2019-08-19 19:10:25 -0700

    Implemented basic constructor, destructor, and method to get the axial coordinate referenced by a mouse click

--- include/inputcontroller.h
+++ include/inputcontroller.h
@@ -2,13 +2,19 @@
 // Created by stott on 8/17/19.
 //
 
-#ifndef THICK_TURTLE_INPUTCONTROLLER_H
-#define THICK_TURTLE_INPUTCONTROLLER_H
+#pragma once
 
+#include "game.h"
+#include "player.h"
+#include <SFML/Window/Event.hpp>
 
 class InputController {
-
+ public:
+    InputController(Game* p_game, Player* p_player);
+    ~InputController();
+    AxialCoordinate GetTileClickedOn(sf::Event::MouseButtonEvent& event);
+ private:
+    Game* m_current_game;
+    Player* m_current_player;
 };
 
-
-#endif //THICK_TURTLE_INPUTCONTROLLER_H
--- src/inputcontroller.cc
+++ src/inputcontroller.cc
@@ -3,3 +3,16 @@
 //
 
 #include "inputcontroller.h"
+
+InputController::InputController(Game *p_game, Player *p_player)
+    : m_current_game(p_game), m_current_player(p_player){
+
+}
+
+InputController::~InputController() {
+
+}
+
+AxialCoordinate InputController::GetTileClickedOn(sf::Event::MouseButtonEvent &event) {
+    return m_current_game->PixelToAxial(event.x, event.y);
+}
